acf24561d5aedba6eac0c92e4bffb8d85bec2728
Alejandro Vera <alexvera0109@gmail.com>
2024-03-23 19:04:22 -0400

    error page commiting

---
+++ src/app/(routes)/[404]/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Button } from "@/components/ui/button";
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="container flex gap-3 flex-col h-[500px] items-center justify-center  ">
+      <h2 className="text-6xl font-bold text-primary">404</h2>
+      <h3 className="text-xl">OOPS! PAGE NOT FOUND</h3>
+      <p className="text-center">
+        The page does not exist or was removed! We suggest you back to home.
+      </p>
+      <Button className="font-bold">Back to Home</Button>
+    </div>
+  );
+}
