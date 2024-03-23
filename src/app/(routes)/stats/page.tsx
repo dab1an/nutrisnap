ade5b9080d425501e71f9be6e7a3c128a441b075
Jacob Schuster <jschuster8765@gmail.com>
2024-03-23 19:03:39 -0400

    work on stats page

---
+++ src/app/(routes)/stats/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Link } from "lucide-react";
+import React from "react";
+import { getMeals, getStats } from "@/lib/data";
+
+async function page() {
+  const stats = await getStats("day");
+  //   const stats = await getMeals();
+
+  return (
+    <div>
+      <Link size={32} href="/">
+        Home
+      </Link>
+
+      <h1>Stats</h1>
+
+      {JSON.stringify(stats, null, 2)}
+    </div>
+  );
+}
+
+export default page;
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres";
 import type { Meal } from "@/types/meal";
+import { createClient } from "@vercel/postgres";
 
 const userId = "dc21b61b-4d40-41ee-bb29-924f73b6cc00"; //todo haha
 
@@ -15,56 +16,41 @@ export async function getMeals() {
  * @param userId - the ID of the user for which to retrieve statistics
  * @returns An object containing the total calories, protein, carbohydrates, fiber, fat, and sugar consumed by the user in the given period
  */
-export async function getStats(period: "day" | "week" | "month" = "day") {
-  const { rows, fields } = await sql`
-  WITH current_period AS (
-    SELECT
-        SUM(calories) AS total_calories,
-        SUM(protein) AS total_protein,
-        SUM(carbs) AS total_carbs,
-        SUM(fat) AS total_fat,
-        SUM(fiber) AS total_fiber,
-        SUM(sugar) AS total_sugar
-        COUNT(*) AS total_meals
-    FROM meal
-    WHERE
-        created_at >= DATE_TRUNC('day', CURRENT_TIMESTAMP) -- Start of the current day
-        AND created_at < DATE_TRUNC('day', CURRENT_TIMESTAMP) + INTERVAL '1 ${period}'
-        AND user_id = ${userId}
-),
-previous_period AS (
-    SELECT
-        SUM(calories) AS total_calories,
-        SUM(protein) AS total_protein,
-        SUM(carbs) AS total_carbs,
-        SUM(fat) AS total_fat,
-        SUM(fiber) AS total_fiber,
-        SUM(sugar) AS total_sugar
-        COUNT(*) AS total_meals
-    FROM meal
-    WHERE
-        created_at >= DATE_TRUNC('day', CURRENT_TIMESTAMP) - INTERVAL '1 ${period}' -- Start of the previous day
-        AND created_at < DATE_TRUNC('day', CURRENT_TIMESTAMP)
-        AND user_id = ${userId}
-)
-SELECT
-    current_period.total_calories AS current_calories,
-    previous_period.total_calories AS previous_calories,
-    current_period.total_protein AS current_protein,
-    previous_period.total_protein AS previous_protein,
-    current_period.total_carbs AS current_carbs,
-    previous_period.total_carbs AS previous_carbs,
-    current_period.total_fat AS current_fat,
-    previous_period.total_fat AS previous_fat,
-    current_period.total_fiber AS current_fiber,
-    previous_period.total_fiber AS previous_fiber,
-    current_period.total_sugar AS current_sugar,
-    previous_period.total_sugar AS previous_sugar
-    current_period.total_meals AS current_num_meals,
-    previous_period.total_meals AS previous_num_meals
-FROM
-    current_period, previous_period;
-`;
+export async function getStats(interval: "day" | "week" | "month" = "day") {
+  const client = await sql.connect();
+  const period = "week";
 
-  return rows;
+  try {
+    const { rows } = await client.query(
+      `
+    SELECT
+    date_trunc($2::text, created_at) AS period,
+    SUM(calories) AS total_calories,
+    SUM(sugar) AS total_sugar,
+    SUM(protein) AS total_protein,
+    SUM(carbs) AS total_carbs,
+    SUM(fat) AS total_fat,
+    SUM(fiber) AS total_fiber,
+    LEAD(SUM(calories), 1) OVER (ORDER BY date_trunc($2::text, created_at)) AS prev_period_calories,
+    LEAD(SUM(sugar), 1) OVER (ORDER BY date_trunc($2::text, created_at)) AS prev_period_sugar,
+    LEAD(SUM(protein), 1) OVER (ORDER BY date_trunc($2::text, created_at)) AS prev_period_protein,
+    LEAD(SUM(carbs), 1) OVER (ORDER BY date_trunc($2::text, created_at)) AS prev_period_carbs,
+    LEAD(SUM(fat), 1) OVER (ORDER BY date_trunc($2::text, created_at)) AS prev_period_fat,
+    LEAD(SUM(fiber), 1) OVER (ORDER BY date_trunc($2::text, created_at)) AS prev_period_fiber
+FROM
+    meal
+WHERE
+    user_id = $1
+GROUP BY
+    date_trunc($2::text, created_at)
+ORDER BY
+    date_trunc($2::text, created_at) DESC;
+      `,
+      [userId, interval]
+    );
+
+    return rows;
+  } finally {
+    // await client.end();
+  }
 }
