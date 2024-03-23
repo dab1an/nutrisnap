04aa9ffeb14f29b2a9a10c56476cea4626c75c80
Jacob Schuster <jschuster8765@gmail.com>
2024-03-23 15:09:52 -0400

    more db stuff

---
+++ src/schemas/meal.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const mealSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  calories: z.number(),
+  protein: z.number(),
+  carbs: z.number(),
+  fat: z.number(),
+  sugar: z.number(),
+  img: z.string(),
+  dateCreated: z.date(),
+  location: z.string(),
+  userId: z.string(),
+});
