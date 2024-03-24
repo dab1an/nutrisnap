af833e92c15937ae6577c20a6a34cf8f9d44ecda
Alejandro Vera <alexvera0109@gmail.com>
2024-03-24 05:18:35 -0400

    camera changes for ios

--- src/app/(routes)/picture/page.tsx
+++ src/app/(routes)/picture/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Navbar from "@/components/Navbar";
+import ImageUploader from "@/components/image-taker";
 import { Button } from "@/components/ui/button";
 import { extractMealInfoFromImage } from "@/lib/ai";
 import { useUserAuth } from "@/utils/hooks/useUserAuth";
@@ -10,19 +11,26 @@ import { FoodCard } from "../dashboard/page";
 const Page = () => {
   const NavBarSpecial = () => {
     return (
-      // <a>
-      //   <p className="font-bold text-2xl">Welcome Back,</p>
-      //   <p className="text-primary font-bold text-2xl">Jacob</p>
-      // </a>
       <a href="/">
         <ArrowLeft />
       </a>
     );
   };
   return (
-    <div className="container flex flex-col gap-10 items-center">
+    <div className="container flex flex-col gap-5 items-center">
       <Navbar special={NavBarSpecial} />
-      <CustomWebcam />
+      <h1 className="text-4xl font-bold text-primary">Take a Picture!</h1>
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-center">
+          Take a picture of your meal and {"we'll"} analyze it to provide you
+          with nutritional information
+        </p>
+        <p className="text-foreground/50 text-sm text-center">
+          Click on the box below to get started. Make sure all food is in frame!
+        </p>
+      </div>
+      <ImageUploader />
+      {/* <CustomWebcam /> */}
     </div>
   );
 };
---
+++ src/components/image-taker.tsx
@@ -0,0 +1,88 @@
+import { extractMealInfoFromImage } from "@/lib/ai";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+const ImageUploader: React.FC = () => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const [imageData, setImageData] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [finished, setFinished] = useState<boolean>(false);
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async () => {
+        const image = new Image();
+        image.src = reader.result as string;
+        image.onload = () => {
+          const canvas = document.createElement("canvas");
+          const maxWidth = 800; // Set your desired maximum width here
+          const maxHeight = 800; // Set your desired maximum height here
+          let width = image.width;
+          let height = image.height;
+
+          if (width > height) {
+            if (width > maxWidth) {
+              height *= maxWidth / width;
+              width = maxWidth;
+            }
+          } else {
+            if (height > maxHeight) {
+              width *= maxHeight / height;
+              height = maxHeight;
+            }
+          }
+
+          canvas.width = width;
+          canvas.height = height;
+          const ctx = canvas.getContext("2d");
+          ctx?.drawImage(image, 0, 0, width, height);
+          const resizedImageData = canvas.toDataURL("image/jpeg", 0.7); // Adjust the quality (0.0 - 1.0) as needed
+          setImageSrc(resizedImageData);
+        };
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const router = useRouter();
+  const handleSubmit = async () => {
+    if (!imageSrc) return;
+    setLoading(true);
+    const response = await extractMealInfoFromImage(imageSrc);
+    setLoading(false);
+    setFinished(true);
+    console.log(response);
+    setImageData(JSON.stringify(response, null, 2));
+  };
+  const RenderButton = () => {
+    if (finished)
+      return <Button onClick={() => router.push("/dashboard")}>Done!</Button>;
+    if (!imageSrc) return null;
+    if (imageSrc && !loading) {
+      return <Button onClick={handleSubmit}>Submit!</Button>;
+    }
+    return (
+      <Button disabled>
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        Processing
+      </Button>
+    );
+  };
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <Input type="file" accept="image/*" capture onChange={handleFileChange} />
+      {imageSrc && (
+        <img src={imageSrc} alt="Selected" style={{ height: "300px" }} />
+      )}
+
+      <RenderButton />
+      {imageData && <pre>{imageData}</pre>}
+    </div>
+  );
+};
+
+export default ImageUploader;
